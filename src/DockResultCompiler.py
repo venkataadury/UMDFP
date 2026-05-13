590777d1c8b64e81eb0bcdaa8c30a25760e295be
Venkata Portable Device <venkata.adury0307@gmail.com>
2026-05-13 15:10:03 -0400

    Docking result compiler (Only PDBQt reader for now)

---
+++ src/DockResultCompiler.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+from rdkit import Chem
+import sys,os
+
+class PDBQtScoreParser:
+    def __init__(self, score_column=-1, score_filter=None, name_filter="Name:"):
+        self.score_data=dict()
+        self.ligand_index=0
+        self.score_column=score_column
+        self.score_filter=score_filter
+        self.name_filter=name_filter
+    
+    def loadFile(self, filename):
+        infile=open(filename,"r")
+        remark_start=False
+        my_name=None
+        my_score=None
+        for l in infile:
+            l=l.strip()
+            if not len(l): continue
+            if not l.startswith("REMARK"):
+                if remark_start and my_score is not None:
+                    if (my_name is None): my_name="ligand_"+str(self.ligand_index)
+                    self.score_data[my_name]=my_score
+                    self.ligand_index+=1
+                remark_start=False
+                my_score=None
+                my_name=None
+                continue
+            else: remark_start=True
+            l=l.replace("REMARK","").strip()
+            
+            is_name=True
+            is_score=True
+            if self.name_filter is not None and l.find(self.name_filter)==-1: is_name=False
+            if self.score_filter is not None and l.find(self.score_filter)==-1: is_score=False
+            if not (is_name or is_score):  continue
+            
+            if is_name:
+                my_name=l.strip().split()[-1].strip()
+                continue
+            if my_score is None:
+                l=l.strip().split()
+                for k in l:
+                    try:
+                        score_num=float(k)
+                        my_score=score_num
+                        break
+                    except: pass
+    
+    def getScoreDict(self): return self.score_data
+    def dumpLigandsToFile(self,input_filename,outfile_obj,namelist):
+        infile=open(input_filename,"r")
+        remark_start=False
+        my_name=None
+        my_score=None
+        lines=[]
+        for l in infile:
+            l=l.strip()
+            if not len(l): continue
+            if not l.startswith("REMARK"):
+                if remark_start and my_score is not None:
+                    if (my_name is None): my_name="ligand_"+str(self.ligand_index)
+                    self.ligand_index+=1
+                lines.append(l)
+                remark_start=False
+                continue
+            else:
+                if not remark_start:
+                    print(my_name,"will be matched to",namelist)
+                    if my_name in namelist:
+                        for m in lines: outfile_obj.write(m+"\n")
+                    my_score=None
+                    my_name=None
+                    lines=[]
+                remark_start=True
+            l=l.replace("REMARK","").strip()
+            
+            is_name=True
+            is_score=True
+            if self.name_filter is not None and l.find(self.name_filter)==-1: is_name=False
+            if self.score_filter is not None and l.find(self.score_filter)==-1: is_score=False
+            if is_name: my_name=l.strip().split()[-1].strip()
+            
+            lines.append(l)
