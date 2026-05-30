ac244feb99f153ce4ac75989e368e36f8225d328
Venkata Sai Sreyas Adury <venkata.adury0307@gmail.com>
2026-05-30 15:32:06 -0400

    Restore Similarity Search CLI behaviour

--- rdkit_src/src/SimilarityQueryCLI.cpp
+++ rdkit_src/src/SimilarityQueryCLI.cpp
@@ -6,7 +6,5 @@
 int main(int argc, char** argv)
 {
     SimilarityQueryJobOutput* output = SimilarityQueryJob(std::string(argv[0])).run(argc, argv);
-    std::cout << "\n\n";
-    std::cout << (*output) << "\n";
     return output->getExitCode();
 }
